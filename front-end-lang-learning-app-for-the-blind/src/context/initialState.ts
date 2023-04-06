2a3ea6feffd48129bd02410d005c7c568fc7f70e
Gabi <gabidiac11@gmail.com>
2023-04-06 23:48:33 +0300

    added story models and documentation for the first pilot story for the feature

--- front-end-lang-learning-app-for-the-blind/src/context/initialState.ts
+++ front-end-lang-learning-app-for-the-blind/src/context/initialState.ts
@@ -1,5 +1,53 @@
-import { StateType } from "./ctxTypes";
+import { StateType, UserStory } from "./ctxTypes";
+
+const words = [
+    // Pronouns
+    { text: "Она", shortTranslation: "She", longTranslation: "" },
+    { text: "Он", shortTranslation: "He", longTranslation: "" },
+    { text: "Они", shortTranslation: "They", longTranslation: "" },
+    { text: "Мы", shortTranslation: "We", longTranslation: "" },
+    { text: "Тебе", shortTranslation: "To you", longTranslation: "" },
+    { text: "Мне", shortTranslation: "To me", longTranslation: "" },
+  
+    // Family
+    { text: "Мама", shortTranslation: "Mom", longTranslation: "" },
+    { text: "Папа", shortTranslation: "Dad", longTranslation: "" },
+    { text: "Сестра", shortTranslation: "Sister", longTranslation: "" },
+    { text: "Брат", shortTranslation: "Brother", longTranslation: "" },
+    { text: "Дочь", shortTranslation: "Daughter", longTranslation: "" },
+    { text: "Сын", shortTranslation: "Son", longTranslation: "" },
+    { text: "Родители", shortTranslation: "Parents", longTranslation: "" },
+    { text: "Бабушка", shortTranslation: "Grandmother", longTranslation: "" },
+    { text: "Дедушка", shortTranslation: "Grandfather", longTranslation: "" },
+  
+    // Temporal
+    { text: "Вчера", shortTranslation: "Yesterday", longTranslation: "" },
+    { text: "Сегодня", shortTranslation: "Today", longTranslation: "" },
+    { text: "Завтра", shortTranslation: "Tomorrow", longTranslation: "" },
+    { text: "Часто", shortTranslation: "Often", longTranslation: "" },
+    { text: "Года", shortTranslation: "Years", longTranslation: "" },
+    { text: "Месяцы", shortTranslation: "Months", longTranslation: "" },
+    { text: "Дни", shortTranslation: "Days", longTranslation: "" },
+  
+    // Places
+    { text: "Дом", shortTranslation: "Home", longTranslation: "" },
+    { text: "Школа", shortTranslation: "School", longTranslation: "" },
+    { text: "Работа", shortTranslation: "Work", longTranslation: "" },
+    { text: "Магазин", shortTranslation: "Store", longTranslation: "" },
+    { text: "Улица", shortTranslation: "Street", longTranslation: "" },
+    { text: "Дверь", shortTranslation: "Door", longTranslation: "" },
+  
+    // Verbs
+    { text: "Есть", shortTranslation: "To eat", longTranslation: "" },
+    { text: "Пить", shortTranslation: "To drink", longTranslation: "" },
+    { text: "Гулять", shortTranslation: "To walk", longTranslation: "" },
+    { text: "Работать", shortTranslation: "To work", longTranslation: "" },
+    { text: "Учиться", shortTranslation: "To study", longTranslation: "" },
+    { text: "Смотреть", shortTranslation: "To watch", longTranslation: "" },
+    { text: "Играть", shortTranslation: "To play", longTranslation: ""}
+];
 
 export default {
-    language: "en"
+    language: "en",
+    userStories: []
 } as StateType;
