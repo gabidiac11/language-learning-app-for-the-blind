593aa12cdd9ceb9ce146b0e77d8f1ee4477f7f2d
Gabriel Diac <gabriel.diac@maxcode.net>
2023-05-04 16:59:41 +0300

    added todo's

--- front-end-lang-learning-app-for-the-blind/src/pages/autheticated/EpiloguePage/EpilogueStartPage.tsx
+++ front-end-lang-learning-app-for-the-blind/src/pages/autheticated/EpiloguePage/EpilogueStartPage.tsx
@@ -17,6 +17,7 @@ const EpilogueStartPage = () => {
         {!error && data && epilogueId && (
           <div className="view-content">
             <h1> Epilogue: {data.epilogue.name} </h1>
+            {/* TODO: implement reading of story */}
             <h5> {data.epilogue.textStoryTale} </h5>
             {data.timeSummaryCompleted && (
               <ButtonContinueToQuiz blockProgressId={data.id} />
@@ -27,5 +28,18 @@ const EpilogueStartPage = () => {
     </div>
   );
 };
+
+/**
+ * TODO:
+ * 0.1 implement initial stories with epilogue summary started??
+ * 0.1 implement start page with start summary and start/continue quiz (locked or what not)
+ * 1. implement GET id -> check if epilogue unlocked
+ * 2. implement POST id -> mark summary completed
+ * 3. extract logic for choosing questions by probabilities to a separate service to be shared with building block
+ *    MAKE SURE - to provide different parameters for building blocks separate from epilogue
+ *    MAKE SURE - maybe provide diffrent parameters based on number of words of a block 
+ * 4. retest buidling block 
+ * 5. start working 
+ */
 
 export default EpilogueStartPage;
