c821905cb683467478db0279f350b55285a70597
Gabi <gabidiac11@gmail.com>
2023-05-11 15:14:14 +0300

    added a todo for logger

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,4 +1,5 @@
 export function log(value: any, obj?: any) {
+  // TODO: I should find some cloud service for this one
   if (obj) {
     const _obj = JSON.stringify(obj, null, 2);
     console.log(value, _obj);
