3e31ddd3f3483d8dfa8c53857fa09a5adc0f3552
Gabi <gabidiac11@gmail.com>
2023-05-22 05:29:28 +0300

    changed database for production

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,10 +3,3 @@ export const STAGE = process.env.REACT_APP_STAGE;
 export const BASE_URL = (() => {
   return `${window.location.origin}/api/`;
 })();
-
-declare global {
-  interface Window {
-    cheat_correctEpilogueOptions?: { [key: string]: true };
-    cheat_correctBlockOptions?: { [key: string]: true };
-  }
-}
